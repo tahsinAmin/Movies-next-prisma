bfdfccd8738c8e242de8a298a35463192304561a
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-11-01 15:01:05 +0600

    Complete

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,67 @@
-import Head from 'next/head'
+import Head from "next/head";
+import {useState} from 'react'
+import Link from 'next/link'
 
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+import {PrismaClient} from '@prisma/client'
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+const prisma = new PrismaClient()
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+export default function Home({ data }) {
+    const [formData, setFormData] = useState({})
+    const [movies, setMovies] = useState(data)
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+    async function saveMovie(e) {
+        e.preventDefault()
+        setMovies([...movies, formData])
+        const response = await fetch('/api/movies', {
+            method: 'POST',
+            body: JSON.stringify(formData)
+        })
+        
+        return await response.json()
+    }
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+    return (
+        <div>
+            <Head>
+                <title>Movie list</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+            <main>
+                <ul >
+                    {movies.map(item => (
+                        <li key="item.id">
+                            <span><strong>{item.title}</strong></span>
+                            <span>{item.year}</span>
+                            <span>{item.descritpion}</span>
+                            <Link href={`/movies/${item.slug}`}>
+                                <a>More about this movie</a>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                <form className="" onSubmit={saveMovie}>
+                    <input type="text" placeholder="Title" name="title" onChange={e => setFormData({ ...formData, title: e.target.value })}/>
+                    <input type="text" placeholder="Year" name="year" onChange={e => setFormData({ ...formData, year: +e.target.value })} />
+                    <textarea name="descritpion" id="" cols="30" rows="10" placeholder="descritpion" onChange={e => setFormData({ ...formData, descritpion: e.target.value })} />
+                    <input type="text" placeholder="Slug" name="slug" onChange={e => setFormData({ ...formData, slug: e.target.value })} />
+                    <button type="submit">Add movie</button>
+                </form>
+
+            </main>
         </div>
-      </main>
+    );
+}
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+export async function getServerSideProps() {
+
+    const movies = await prisma.movie.findMany()
+
+    return {
+        props: {
+            data: movies
+        }
+    }
 }
